f5161b1cf361638912ca961082e99c43317de49e
Matthias Pfeil <matthias.pfeil@wwu.de>
2016-11-30 23:14:40 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# alexa-wer-hat-es-gesagt
+# alexa-wer-hat-es-gesagt [![Version](https://img.shields.io/badge/Version-1.0.0-brightgreen.svg)](https://img.shields.io/badge/Version-1.0.0-brightgreen.svg) [![Certification Status](https://img.shields.io/badge/Status-Certification-orange.svg)](https://img.shields.io/badge/Status-Live-orange.svg) [![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://img.shields.io/badge/license-MIT-blue.svg)
+
+> Wenn man keine Ahnung hat: Einfach mal Fresse halten! - Dieter Nuhr
+
+Alexa testet dein Wissen rundum bekannte Zitate! 
+
+## Entstehung
+Diese Alexa Fähigkeit (Skill) ist im Rahmen des Alexa Skill Building Berlin Bootcamp entstanden. Dabei handelt es sich um die einfachste Art einer Alexa Fähigkeit. Diese Fähigkeit basiert auf dem [Alexa Trivia Skill](https://github.com/alexa/skill-sample-nodejs-trivia).
+
+## Äußerungen (Utterances)
+Mit den folgenden Aussagen, kannst du Wer hat es gesagt steuern:
+```
+die Antwort ist {Answer}
+meine Antwort ist {Answer}
+ist die Antwort {Answer}
+{Answer} ist meine Antwort
+{Answer}
+
+Spiel starten
+neues Spiel
+starten
+neues Spiel starten
+
+weiß ich nicht
+weiß nicht
+überspringen
+das weiß ich nicht
+wer weiß
+diese Frage weiß ich nicht
+die weiß ich nicht
+keine Ahnung
+```
+Die Äußerungen müssen in der Entwicklung spezifiziert werden. Die Äußerungen findest du unter `src/Utterances_de_DE.txt`.
+
+Bevor du die Äußerungen sagst und Alexa dich mit einer Fussballweisheit bespaßt, musst du die Fähigkeit öffnen/aktivieren. Dies kannst du folgendermaßen machen:
+```
+Alexa, öffnen wer hat es gesagt
+Alexa, starte wer hat es gesagt
+```
+Danach kannst du dann die oben genannten Äußerungen benutzen.
---
+++ src/questions.js
@@ -0,0 +1,189 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_DE_DE" : [
+        {
+            "Zwei Dinge sind unendlich, das Universum und die menschliche Dummheit, aber bei dem Universum bin ich mir noch nicht ganz sicher? ": [
+                "Albert Einstein",
+                "Alfred Kleiner",
+                "Heinrich Burkhardt",
+                "Paul Drude",
+                "Max Planck"
+            ]
+        },
+        {
+            "Das eigentliche Studium der Menschheit ist der Mensch? ": [
+                "Johann Wolgang von Goethe",
+                "Hermann Hesse",
+                "Franz Schubert",
+                "Arthur Schopenhauer",
+                "Friedrich Nietzsche",
+                "Thomas Mann"
+            ]
+        },
+        {
+            "Alle Autorität, die ich besitze, beruht einzig darauf, dass ich weiß, was ich nicht weiß? ": [
+                "Sokrates",
+                "Xerxes",
+                "Platon",
+                "Aristoteles"
+            ]
+        },
+        {
+            "Das Verhängnis unserer Kultur ist, dass sie sich materiell viel stärker entwickelt hat als geistig? ": [
+                "Albert Schweitzer",
+                "Wilhelm Busch",
+                "Bertolt Brecht",
+                "Heinrich Heine"
+            ]
+        },
+        {
+            "Der Mensch hat mehr von einem Affen als so mancher Affe? ": [
+                "Friedrich Nietzsche",
+                "Gustav Heinemann",
+                "Friedrich Schiller",
+                "Bertolt Brecht"
+            ]
+        },
+        {
+            "Demokratie ist die Regierung des Volkes durch das Volk für das Volk? ": [
+                "Abraham Lincoln",
+                "Winston Churchill",
+                "Henry Ford",
+                "Benjamin Franklin"
+            ]
+        },
+        {
+            "Reich wird man erst durch Dinge, die man nicht begehrt? ": [
+                "Mahatma Gandhi",
+                "Konfuzius",
+                "Ovid",
+                "Platon"
+            ]
+        },
+        {
+            "Kinder sind das lieblichste Pfand der Ehe, sie binden und erhalten das Band der Liebe? ": [
+                "Martin Luther",
+                "Friedrich Schiller",
+                "Arthur Schopenhauer",
+                "Alexander von Humboldt"
+            ]
+        },
+        {
+            "Wer kämpft, kann verlieren. Wer nicht kämpft, hat schon verloren? ": [
+                "Bertolt Brecht",
+                "Hermann Hesse",
+                "Heinrich Heine",
+                "Friedrich Nietzsche"
+            ]
+        },
+        {
+            "Ich bin wie ich bin. Die einen kennen mich, die anderen können mich? ": [
+                "Konrad Adenauer",
+                "Willy Brandt",
+                "Helmut Schmidt",
+                "Angela Merkel"
+            ]
+        },
+        {
+            "Der Bankraub ist eine Initiative von Dilettanten. Wahre Profis gründen eine Bank? ": [
+                "Bertolt Brecht",
+                "Hermann Hesse",
+                "Heinrich Heine",
+                "Friedrich Nietzsche"
+            ]
+        },
+        {
+            "Die Zehn Gebote sind deswegen so kurz und logisch, weil sie ohne Mitwirkung von Juristen zustande gekommen sind? ": [
+                "Charles de Gaulle",
+                "Winston Churchill",
+                "Konrad Adenauer",
+                "Richard Nixon"
+            ]
+        },
+        {
+            "Hier bin ich Mensch, hier darf ich's sein? ": [
+                "Johann Wolfgang von Goethe",
+                "Wilhelm Busch",
+                "Heinrich Heine",
+                "Robert Lembke"
+            ]
+        },
+        {
+            "Ein Kluger bemerkt alles, ein Dummer macht über alles seine Bemerkungen? ": [
+                "Heinrich Heine",
+                "Hermann Hesse",
+                "Wilhelm Busch",
+                "Robert Lembke"
+            ]
+        },
+        {
+            "Wer zur Quelle will, muss gegen den Strom schwimmen? ": [
+                "Hermann Hesse",
+                "Arthur Schopenhauer",
+                "Thomas Mann",
+                "Friedrich Schiller"
+            ]
+        },
+        {
+            "Bildung lässt sich nicht downloaden? ": [
+                "Günther Jauch",
+                "Hape Kerkeling",
+                "Horst Schlämmer",
+                "Steffen Hallaschka"
+            ]
+        },
+        {
+            "Wer fragt, ist ein Narr für eine Minute. Wer nicht fragt, ist ein Narr sein Leben lang? ": [
+                "Konfuzius",
+                "Seneca",
+                "Ovid",
+                "Epikur"
+            ]
+        },
+        {
+            "Wenn man keine Ahnung hat: Einfach mal Fresse halten? ": [
+                "Dieter Nuhr",
+                "Mario Barth",
+                "Harald Schmidt",
+                "Carolin Kebekus"
+            ]
+        },
+        {
+            "Was ist die Mehrheit? Mehrheit ist der Unsinn! Verstand ist stets bei wenigen nur gewesen? ": [
+                "Friedrich Schiller",
+                "Hermann Hesse",
+                "Heinrich Heine",
+                "Friedrich Nietzsche"
+            ]
+        },
+        {
+            "Man kann es so oder so machen. Ich bin für so? ": [
+                "Gerhard Schröder",
+                "Angela Merkel",
+                "Edmund Stoiber",
+                "Helmut Kohl"
+            ]
+        },
+        {
+            "Es kommt nicht darauf an, mit dem Kopf durch die Wand zu rennen, sondern mit den Augen die Tür zu finden? ": [
+                "Werner von Siemens",
+                "Albert Einstein",
+                "Herny Ford",
+                "Theodor Fontane"
+            ]
+        },
+        {
+            "Sorgen ertrinken nicht im Alkohol, sie können schwimmen? ": [
+                "Heinz Rührmann",
+                "Hertha Feiler",
+                "Eduard von Schleich",
+                "Peter Ustinov"
+            ]
+        }
+    ]
+};
